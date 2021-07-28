b6886cf5d2d11bcaee2e7745fcac1db6a3683a94
kaorinS <pluvia.seed@icloud.com>
2021-07-28 13:22:31 +0900

    タスク追加

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,18 @@ import Vue from 'vue'
 new Vue({
     el: '#app',
     data: {
-        newItem: ""
+        newItem: "",
+        todos: []
     },
     methods: {
         addItem: function (e) {
-            alert();
+            // alert();
+            if (this.newItem === '') return; //タスク未入力の場合は追加しない
+            let todo = {
+                item: this.newItem
+            };
+            this.todos.push(todo); //todosの配列にtodoを追加する
+            this.newItem = ''  //タスク追加後、入力フォームを空にする
         }
     }
 })
